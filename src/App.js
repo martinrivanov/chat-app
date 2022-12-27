8ee8fa4bf0b79240266a71156f9b71a98f510dae
martinrivanov <martinrivanov43@gmail.com>
2022-12-28 00:28:11 +0200

    Setting up firebase in the project7

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import { auth } from './firebase/setup';
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ src/firebase/setup.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAIUqTResh55cqf-VLca3YVI_f9RuhVHz4",
+    authDomain: "graduation-project-f1f53.firebaseapp.com",
+    projectId: "graduation-project-f1f53",
+    storageBucket: "graduation-project-f1f53.appspot.com",
+    messagingSenderId: "1052485363656",
+    appId: "1:1052485363656:web:0b8ab8eae92aba73e702fd"
+  });
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export {auth, firestore};
